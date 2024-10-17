757738ae9bf0dec675bc2d1fa7cfe36db9b3853b
ROUX SACHA <sacha.work38@gmail.com>
2024-10-17 18:53:07 +0200

    Conception du composant header et footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,51 @@
+import "../styles/Footer.css";
+import logoWhite from '../assets/logo-white.png'
+import { FaLinkedin } from "react-icons/fa6";
+import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+
+
+
+
+export default function Footer(){
+    return (
+        <footer>
+            <div className="high-footer">
+                <p>Proposer les meilleurs aéronefs du marché</p>
+                <img src={logoWhite} alt="logo" />
+                <div className="reseau">
+                    <FaLinkedin className="reseau-picture" size={40}/>
+                    <FaSquareXTwitter className="reseau-picture" size={40}/>
+                    <FaInstagram className="reseau-picture" size={40}/>
+                </div>
+            </div>
+            <div className="bottom-footer">
+                <div>
+                    <h2>Liens rapides</h2>
+                    <ul>
+                        <li><a href="">Accueil</a></li>
+                        <li><a href="">Catalogue d'aéronefs</a></li>
+                        <li><a href="">Prendre rendez-vous</a></li>
+                        <li><a href="">Services et maintenance</a></li>
+                    </ul>
+                </div>
+                <div className="left-separator">
+                    <h2>Informations Légales</h2>
+                    <ul>
+                        <li><a href="">Mentions légales</a></li>
+                        <li><a href="">Politique de confidentialité</a></li>
+                        <li><a href="">Conditions générales de vente (CGV)</a></li>
+                    </ul>
+                </div>
+                <div className="left-separator">
+                    <h2>Coordonnées</h2>
+                    <ul>
+                        <li>Adresse : 123 Rue des Ailes, 75008 Paris, France</li>
+                        <li>Téléphone : +33 1 23 45 67 89</li>
+                        <li>E-mail : contact@airconcess.com</li>
+                    </ul>
+                </div>
+            </div>
+        </footer>
+    );
+}
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import '../styles/Header.css'
+import logoWhite from '../assets/logo-white.png'
+import logoBlack from '../assets/logo-black.png'
+
+export default  function Header({color}){
+    let logo = logoWhite
+
+    if (color === "white-black" || color === "transparent-black") {
+        logo = logoBlack
+    }
+
+    return (
+        <header className={`${color}`}>
+            <img src={logo} alt="Logo" />
+            <div className="right-header">
+                <ul>
+                    <li><a href="">Nos aéronefs</a></li>
+                    <li><a href="">Nos services</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">A propos</a></li>
+                </ul>
+                <form action="">
+                    <button>Connexion</button>
+                </form>
+            </div>
+        </header>
+    );
+}
