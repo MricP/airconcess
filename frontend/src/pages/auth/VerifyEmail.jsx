c1ee31be572b509f9ab78ba0c9136633ba23f280
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2024-11-16 05:57:19 +0100

    feat() : css page validation email

--- frontend/src/pages/auth/VerifyEmail.jsx
+++ frontend/src/pages/auth/VerifyEmail.jsx
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { verifyEmail } from '../../services/auth';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
+import TwoColumnLayout from '../../components/auth/TwoColumnLayout';
+import DarkButton from '../../components/DarkButton';
+import '../../styles/auth/VerifyEmail.css';
 
 export default function VerifyEmail() {
     const [errorHtml, setErrorHtml] = useState(null);
     const [responseMessage, setResponseMessage] = useState('');
     const location = useLocation();
+    const navigate = useNavigate();
+
+    const verifyEmailImg = '/assets/auth/validation-img.jpg';
+    const airconcessLogo = '/assets/logo-black.png';
+    const airconcessLogoPlane = '/assets/airconcess-logo-plane.png';
 
     useEffect(() => {
         const queryParams = new URLSearchParams(location.search);
@@ -30,18 +38,50 @@ export default function VerifyEmail() {
         }
     }, [location.search]);
 
-    return (
-        <div>
-            <h2>Vérifiez votre e-mail</h2>
-            <Link to="/sign-in">Se connecter</Link>
-            {responseMessage && <p>{responseMessage}</p>}
+    const handleRetour = () => {
+        navigate('/');
+    }
 
-            {errorHtml && (
-                <div
-                    dangerouslySetInnerHTML={{ __html: errorHtml }}
-                    style={{ color: 'red', marginTop: '20px' }}
-                />
-            )}
-        </div>
+    return (
+        <TwoColumnLayout
+            leftContainerChildren={
+                <div className="left-container">
+                    <div className="logos-img-container">
+                        <img src={verifyEmailImg} alt="Verify Email" className="verify-email-img" />
+                    </div>
+                </div>
+            }
+            rightContainerChildren={
+                responseMessage ? (
+                    <div className="right-container">
+                        <div className="logos-img-container">
+                            <img src={airconcessLogo} alt="AirConcess" className="airconcess-logo" />
+                        </div>
+                        <div className="right-container">
+                            <h2 className="right-container-title">Votre email a bien été validé !</h2>
+                            <p className='right-container-para'>Vous pouvez maintenant acheter, louer et réserver.</p>
+                            <DarkButton text="Retourner à l’accueil" use={() => handleRetour()} className="verify-email-retour" />
+                            {responseMessage && <p className="response-message">{responseMessage}</p>}
+                            {errorHtml && (
+                                <div
+                                    dangerouslySetInnerHTML={{ __html: errorHtml }}
+                                    className="error-html"
+                                />
+                            )}
+                        </div>
+                    </div>
+                ) : (
+                    <div className="right-container">
+                        <div className="logos-img-container">
+                            <img src={airconcessLogoPlane} alt="AirConcess" className="plane-airconcess-logo" />
+                            <img src={airconcessLogo} alt="AirConcess" className="airconcess-logo" />
+                        </div>
+                        <h2 className='right-container-title'>tu n'as rien à faire ici !</h2>
+                        <p className='right-container-para'>Veuillez vérifier votre email.</p>
+                        <DarkButton text="Retour" use={() => handleRetour()} className="verify-email-retour" />
+                    </div>
+                )
+            }
+        />
     );
 }
