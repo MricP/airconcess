40e6d7fe2a3b7b30b0e2ec300b3cc5dfc524a3d4
ROUX SACHA <sacha.work38@gmail.com>
2024-11-19 21:18:39 +0100

    Réorganisation des pages + implémentation des boutons up et down

--- frontend/src/pages/PageServices.jsx
+++ frontend/src/pages/PageServices.jsx
@@ -1,20 +1,10 @@
 import Template from '../components/Template';
-import TrainingService from '../components/TrainingService';
-import MaintenanceService from '../components/MaintenanceService';
-import "../styles/PageServices.css";
+import TrainingService from '../components/our-services/TrainingService';
+import MaintenanceService from '../components/our-services/MaintenanceService';
+import "../styles/our-services/PageServices.css";
 import { useEffect } from 'react';
 
 export default function PageServices() {
-
-    useEffect(() => {
-        // Appliquer overflow:hidden pour masquer la scrollbar principale
-        document.body.style.overflow = "hidden";
-
-        // Nettoyage : Rétablir la scrollbar quand on quitte la page
-        return () => {
-            document.body.style.overflow = "";
-        };
-    }, []);
     
     return (
         <Template>
