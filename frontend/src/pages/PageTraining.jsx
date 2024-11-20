18bbde577b7052163442ab5225e7edc8b3b7526e
ROUX SACHA <sacha.work38@gmail.com>
2024-11-20 15:56:40 +0100

    Début du second composant de la page training

--- frontend/src/pages/PageTraining.jsx
+++ frontend/src/pages/PageTraining.jsx
@@ -1,11 +1,13 @@
-import FirstTrainingComponent from "../components/training/FirstTrainingComponent";
 import "../styles/training/PageTraining.css";
+import FirstTrainingComponent from "../components/training/FirstTrainingComponent";
+import SecondTrainingComposant from "../components/training/SecondTrainingComposant";
 
 export default function PageTraining() {
     return (
         <main>
             <div className="page-training">
                 <FirstTrainingComponent />
+                <SecondTrainingComposant />
             </div>
         </main>
     )
