6b7fb240b0a58256220da6dff9c97e17e5af4db5
ROUX SACHA <sacha.work38@gmail.com>
2024-11-20 15:21:32 +0100

    Création du 1er composant training

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import PageServices from './pages/PageServices.jsx';
+import PageTraining from './pages/PageTraining.jsx';
+import Template from './components/Template.jsx';
 
 function App() {
   
   return (
+    
     <BrowserRouter>
+    <Template>
     <Routes>
       <Route path='/services' element={<PageServices />} />
+      <Route path='/training' element={<PageTraining />} />
     </Routes>
     <ToastContainer />
+    </Template>
   </BrowserRouter>
    
   );
---
+++ frontend/src/pages/PageTraining.jsx
@@ -0,0 +1,12 @@
+import FirstTrainingComponent from "../components/training/FirstTrainingComponent";
+import "../styles/training/PageTraining.css";
+
+export default function PageTraining() {
+    return (
+        <main>
+            <div className="page-training">
+                <FirstTrainingComponent />
+            </div>
+        </main>
+    )
+}
