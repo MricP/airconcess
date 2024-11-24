a9ce70a18b5f33740753ca5db617e67f586a7390
ROUX SACHA <sacha.work38@gmail.com>
2024-11-25 00:05:31 +0100

    Début de la création du premier composant

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import PageServices from './pages/PageServices.jsx';
 import PageTraining from './pages/PageTraining.jsx';
 import Template from './components/Template.jsx';
+import PageMaintenance from './pages/PageMaintenance.jsx';
 
 function App() {
   
@@ -14,6 +15,7 @@ function App() {
     <Routes>
       <Route path='/services' element={<PageServices />} />
       <Route path='/training' element={<PageTraining />} />
+      <Route path='/maintenance' element={<PageMaintenance />} />
     </Routes>
     <ToastContainer />
     </Template>
---
+++ frontend/src/pages/PageMaintenance.jsx
@@ -0,0 +1,10 @@
+import "../styles/maintenance/PageMaintenance.css";
+import FirstMaintenanceComponent from "../components/maintenance/FirstMaintenanceComponent"
+
+export default function PageMaintenance() {
+    return (
+        <div className="page-maintenance">
+            <FirstMaintenanceComponent />
+        </div>
+    )
+}
