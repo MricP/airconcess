5fd3dfa478e6efd1bfa9de3b8f536ff7484cd0e3
ROUX SACHA <sacha.work38@gmail.com>
2024-11-27 00:04:29 +0100

    Fin du 1er composant de la page maintenance

--- frontend/src/components/maintenance/FirstMaintenanceComponent.jsx
+++ frontend/src/components/maintenance/FirstMaintenanceComponent.jsx
@@ -6,8 +6,8 @@ export default function FirstMaintenanceComponent(){
             <img src="assets/maintenance3.jpg" alt="" />
             <section>
                 <h2>Nos Services <br></br>De Maintenance</h2>
-                <p>Vous souhaitez prendre soin de vos avions,  c’est possible grâce à nos services de maintenance.
-                Découvrez nos différentes offres allant d’un simple nettoyage à une révision complète de toute votre collection.</p>
+                <p>Vous souhaitez prendre soin de vos avions,  c’est possible grâce à nos services de maintenance.</p>
+                <p>Découvrez nos différentes offres allant d’un simple nettoyage à une révision complète de toute votre collection.</p>
             </section>
         </div>
     )
