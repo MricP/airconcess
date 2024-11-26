fdc0c498a96e740f9d6aef17ead13ac63e860bd7
ROUX SACHA <sacha.work38@gmail.com>
2024-11-27 00:43:18 +0100

    Fin du 2eme composant de la page maintenance

--- frontend/src/pages/PageMaintenance.jsx
+++ frontend/src/pages/PageMaintenance.jsx
@@ -1,10 +1,12 @@
 import "../styles/maintenance/PageMaintenance.css";
 import FirstMaintenanceComponent from "../components/maintenance/FirstMaintenanceComponent"
+import SecondMaintenanceComponent from "../components/maintenance/SecondMaintenanceComponent";
 
 export default function PageMaintenance() {
     return (
         <div className="page-maintenance">
             <FirstMaintenanceComponent />
+            <SecondMaintenanceComponent />
         </div>
     )
 }
