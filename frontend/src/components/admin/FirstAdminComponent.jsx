325ae3c106a7465730fea94d66293419ec7df707
ROUX SACHA <sacha.work38@gmail.com>
2024-12-02 14:51:44 +0100

    Mise en place du menu cliquable

--- frontend/src/components/admin/FirstAdminComponent.jsx
+++ frontend/src/components/admin/FirstAdminComponent.jsx
@@ -8,24 +8,49 @@ import { FaChevronRight } from "react-icons/fa6";
 
 export default function FirstAdminComponent() {
 
-    const [isClicked, updateClicked] = useState(false)
+    const [isClicked1, updateClicked1] = useState(false)
 
-    const handleMenuClick = () => {
-        updateClicked(!isClicked);
+    const handleMenuClick1 = () => {
+        updateClicked1(!isClicked1);
     };
+
+    const [isClicked2, updateClicked2] = useState(false)
+
+    const handleMenuClick2 = () => {
+        updateClicked2(!isClicked2);
+    };
+
+    const handleElementClick = (event) => {
+        const allElements = document.querySelectorAll(".element");
+        allElements.forEach((el) => el.classList.remove("underline"));
+        event.target.classList.add("underline")
+    }
 
   return (
     <div className="first-admin-component">
         <div className="title-menu">
-            {isClicked ? <FaChevronDown onClick={handleMenuClick} className="chevron"/> : <FaChevronRight onClick={handleMenuClick} className="chevron" />}
+            {isClicked1 ? <FaChevronDown onClick={handleMenuClick1} className="chevron"/> : <FaChevronRight onClick={handleMenuClick1} className="chevron" />}
             <p>Articles</p>
         </div>
 
-        {isClicked && 
+        {isClicked1 && 
             <div className="title-menu-child">
-                <p>&#8226; Ajouter un produit</p>
-                <p>&#8226; Modifier un produit</p>
-                <p>&#8226; Supprimer un produit</p>
+                <p className ="element" onClick={handleElementClick}>&#8226; Ajouter un produit</p>
+                <p className ="element" onClick={handleElementClick}>&#8226; Modifier un produit</p>
+                <p className ="element" onClick={handleElementClick}>&#8226; Supprimer un produit</p>
+            </div>
+        }
+
+        <div className="title-menu">
+            {isClicked2 ? <FaChevronDown onClick={handleMenuClick2} className="chevron"/> : <FaChevronRight onClick={handleMenuClick2} className="chevron" />}
+            <p>Utilisateurs</p>
+        </div>
+
+        {isClicked2 && 
+            <div className="title-menu-child">
+                <p className ="element" onClick={handleElementClick}>&#8226; Ajouter un utilisateur</p>
+                <p className ="element" onClick={handleElementClick}>&#8226; Modifier un utilisateur</p>
+                <p className ="element" onClick={handleElementClick}>&#8226; Supprimer un utilisateur</p>
             </div>
         }
         
