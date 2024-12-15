23dd19e6a87deafce22f8966a9ade32a48d1babf
matheo.flores <matheoflores26@gmail.com>
2024-12-15 16:12:12 +0100

    temp

--- frontend/src/components/sub-training/FormSubTraining.jsx
+++ frontend/src/components/sub-training/FormSubTraining.jsx
@@ -3,10 +3,29 @@ import DarkButton from '../general/DarkButton'
 import { Steps } from 'rsuite';
 import { IoCheckmarkDoneOutline } from "react-icons/io5";
 import { GrFormPrevious } from "react-icons/gr";
+import { useForm } from "react-hook-form";
+
+import InfoFormFieldset from "../appointment/InfoFormFieldset"
 
 import "../../styles/sub-training/FormSubTraining.css"
 
 function FormSubTraining() {
+  const {register,handleSubmit,watch,setValue, formState: { errors }} = useForm (
+    { defaultValues: {
+        firstName: "",
+        lastName: "",
+        phone: "",
+        email: "",
+        address: "",
+        country: "",
+        city: "",
+        postalCode: "",
+        idCard: null
+    }}
+  );
+
+    const formData = watch();
+
   const [step,updateStep] = useState(0);
 
   function handlePrevStep() {
@@ -18,6 +37,21 @@ function FormSubTraining() {
   function handleNextStep() {
     if(step<5) {
       step == 3 ? updateStep(5) : updateStep(step+1)
+    }
+  }
+
+  function handleStepDisplayed() {
+    switch(step) {
+      case 0:
+        return(
+          <form action="">
+            <InfoFormFieldset formData={formData} register={register} errors={errors} setValue={setValue} withIdCard={true}/>
+          </form>
+        )
+      case 1:
+        return(
+          <div>Step 1</div>
+        )
     }
   }
 
@@ -33,7 +67,7 @@ function FormSubTraining() {
         <Steps.Item icon={<IoCheckmarkDoneOutline style={{ fontSize: 20 ,color:"#5b5b5b"}}/>}/>
       </Steps>
       <div className='content'>
-        {"STEP "+step}
+        {handleStepDisplayed()}
       </div>
       <DarkButton disabled={step === 5} className="next-step" onClick={handleNextStep}>Suivant</DarkButton>
     </div>
