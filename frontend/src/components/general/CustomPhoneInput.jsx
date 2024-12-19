1a81b72f39883db588ce6d4b20fd7f4cc3d051ca
matheo.flores <matheoflores26@gmail.com>
2024-12-19 16:33:33 +0100

    Correction du meme probleme avec le phone input et ajout de l'attribut value qui n'est pas correctement retranscit par le rest. Etape 1 fini

--- frontend/src/components/general/CustomPhoneInput.jsx
+++ frontend/src/components/general/CustomPhoneInput.jsx
@@ -3,11 +3,12 @@ import PhoneInput from 'react-phone-number-input'
 
 import '../../styles/general/PhoneInput.css';
 
-const CustomPhoneInput = forwardRef(({className="",setValue, ...rest},ref) => {
+const CustomPhoneInput = forwardRef(({className="",setValue,value, rest},ref) => {
   return (
     <PhoneInput
         ref={ref}
         className={className}
+        value={value}
         onChange={(value) => {
             setValue(value);
         }}
--- frontend/src/components/general/CustomSelectPicker.jsx
+++ frontend/src/components/general/CustomSelectPicker.jsx
@@ -3,13 +3,14 @@ import { SelectPicker } from "rsuite";
 
 import '../../styles/general/Rsuite-custom.css'; 
 
-const CustomSelectPicker = forwardRef(({className,setValue,data,searchable=false,placeholder=" ",rest},ref) => {
+const CustomSelectPicker = forwardRef(({className,setValue,data,value,searchable=false,placeholder=" ",rest},ref) => {
 
     return (
       <SelectPicker
           className={className}
           ref={ref}
           data={data}
+          value={value}
           searchable={searchable}
           placeholder={placeholder}
           onChange={(val) => {
