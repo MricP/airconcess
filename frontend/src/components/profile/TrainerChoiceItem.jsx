eabbc6502396be24a925fbd0748f2be486d635d3
matheo.flores <matheoflores26@gmail.com>
2025-02-17 11:51:15 +0100

    Debut et fin du frontend de la partie trainer (sans css)

--- frontend/src/components/profile/TrainerChoiceItem.jsx
+++ frontend/src/components/profile/TrainerChoiceItem.jsx
@@ -1,34 +1,70 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState,useEffect } from 'react'
 import '../../styles/profile/TrainerChoiceItem.css'
 import { IoIosArrowForward } from "react-icons/io";
 import TrainingProposal from './TrainingProposal';
 import { FiPlusCircle } from "react-icons/fi";
 import DarkButton2 from "../general/DarkButton2"
-
-export default function TrainerChoiceItem() {
-
-    let profile = '/assets/profile/Jack-Sparrow.png';
-
-    const arrowItem = useRef(null)
-    const Item = useRef(null)
-    const [isOpen,setIsOpen] = useState(false)
-    const [trainingProposals, setTrainingProposals] = useState([{}]);
+import { useForm } from "react-hook-form";
+import { RxCross1 } from "react-icons/rx";
+import { IoIosAddCircleOutline } from "react-icons/io";
+import { IoIosAddCircle } from "react-icons/io";
 
 
-    const handleItemClick = () => {
-        setIsOpen(!isOpen);
-        if(arrowItem.current && Item.current){
-            arrowItem.current.classList.toggle("click");
-        }
+export default function TrainerChoiceItem({trainingData}) {
+    const [proposals, setProposals] = useState([]); // Stocke des objets avec un identifiant unique
+
+    const {register,handleSubmit,watch,setValue, formState: { errors }} = useForm (
+        { defaultValues: {
+            proposals: null, //Format : { IDPROPOSAL: {dateStart:VALUE,dateEnd:VALUE,times: [timeM,timeTu,timeTh,timeWe,timeF]} }
+        }}
+    );
+
+    const formData = watch()
+
+    const [isHovered, setIsHovered] = useState(false);
+
+    const addProposal = () => {
+        const newProposal = {id: "id"+Math.round(Math.random()*10000) }; // Crée un objet unique avec un id
+        setProposals([...proposals, newProposal]);
     }
-
-    const addTrainingProposal = () => {
-        setTrainingProposals([...trainingProposals, {}]); // Ajouter un nouvel élément
+    
+    const removeProposal = (id) => {
+        setProposals((prev) => prev.filter((proposal) => proposal.id !== id));   // Suppression
+        if (formData?.proposals) {
+            // Suppression de la proposation dans le useForm
+            delete formData.proposals[id];
+        }
     };
+    
+    useEffect(() => {
+        let temp = proposals;
+        for(let key in formData.proposals) {
+            temp = [...temp,{id:key}]
+            setProposals(temp)
+        }
+    },[]);
+    
+    // let profile = '/assets/profile/Jack-Sparrow.png';
+    // const arrowItem = useRef(null)
+    // const Item = useRef(null)
+    // const [isOpen,setIsOpen] = useState(false)
+    // const [trainingProposals, setTrainingProposals] = useState([{}]);
+
+
+    // const handleItemClick = () => {
+    //     setIsOpen(!isOpen);
+    //     if(arrowItem.current && Item.current){
+    //         arrowItem.current.classList.toggle("click");
+    //     }
+    // }
+
+    // const addTrainingProposal = () => {
+    //     setTrainingProposals([...trainingProposals, {}]); // Ajouter un nouvel élément
+    // };
 
     return (
-        <div className={`trainerChoiceItem-container`} ref={Item}>
-            <div className='trainerChoiceItem-userMainInfos-container' onClick={handleItemClick}>
+        <div className={`trainerChoiceItem-container`} >
+            {/* <div className='trainerChoiceItem-userMainInfos-container' onClick={handleItemClick}>
                 <div className='trainerChoiceItem-userMainInfos'>
                     <img className='trainerChoiceItem-userMainInfos-profilePic' src={profile} alt="photo" />
                     <p>Pirrera</p>
@@ -39,14 +75,48 @@ export default function TrainerChoiceItem() {
             {isOpen === true && (
                 <>
                     {trainingProposals.map((proposal) => (
-                        <TrainingProposal key={proposal.id} />
+                        
                         
                     ))}
                     <DarkButton2 className={"darkButton"} text={"Valider"}/>
                     <FiPlusCircle onClick={addTrainingProposal} className='TrainerChoiceItem-plusButton' size={25} color='var(--button-color)' />
                 </>
-            )}
+            )} */}
+        
+        
+        <div>
+            <p>Préférences client</p>
+            <p>LES PREFERENCES</p>
         </div>
+        
+
+        
+        <div className='proposals' id={proposals.length === 0 ? "nothing" : ""}>
+            {proposals.length === 0 ? 
+                <div style={{display:"flex",justifyContent:"center", alignItems:"center"}}>Aucune proposition</div> :               
+            proposals?.map((proposal) => (
+                <div className="proposal" key={proposal.id}>
+                    <p>{proposal.id}</p>
+                    <TrainingProposal proposId={proposal.id} formData={formData} errors={errors} setValue={setValue} register={register}/>
+                        
+                    <RxCross1  className='button-del' onClick={() => removeProposal(proposal.id)}>
+                        Supprimer
+                    </RxCross1 > 
+                </div>
+            ))}
+
+            <div className="icon-container" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>   
+                <IoIosAddCircle
+                    className={`add-time-slot ${isHovered ? "visible" : "hidden"} ${proposals.length < 3 ? "" : "invisible"}`}
+                    onClick={proposals.length < 5 ? addProposal : null}
+                />
+                <IoIosAddCircleOutline
+                    className={`add-time-slot ${!isHovered ? "visible" : "hidden"} ${proposals.length < 3 ? "" : "invisible"}`}
+                    onClick={proposals.length < 5 ? addProposal : null}
+                />
+            </div>
+        </div>
+    </div>
     )
 }
 
--- frontend/src/components/profile/TrainingProposal.jsx
+++ frontend/src/components/profile/TrainingProposal.jsx
@@ -1,38 +1,69 @@
 import React from 'react'
 import CustomDatePicker from "../general/CustomDatePicker"
 import "../../styles/profile/TrainingProposal.css"
+import CustomTimePicker from '../general/CustomTimePicker'
 
-function TrainingProposal() {
+function TrainingProposal({proposId,formData,errors,setValue,register}) {
   return (
-    <div className='TrainingProposal-container'>
-
-        <h4>Préférences utilisateur</h4>
-        <p>Date : </p>
-        <p>Horaires :</p>
-        <table className='TrainingProposal-border'>
+    <div className='tp-container'>
+        <table className='tp-table'>
             <tbody>
                 <tr className='TrainingProposal-border'>
                     <th>Jours</th>
-                    <th>L</th>
-                    <th>M</th>
-                    <th>M</th>
-                    <th>J</th>
-                    <th>V</th>
+                    <th>Lundi</th>
+                    <th>Mardi</th>
+                    <th>Mercredi</th>
+                    <th>Jeudi</th>
+                    <th>Vendredi</th>
                 </tr>        
                 <tr className='TrainingProposal-border'>
                     <th>Horaires</th>
-                    <td><p contentEditable="true"></p></td>
-                    <td><p contentEditable="true"></p></td>
-                    <td><p contentEditable="true"></p></td>
-                    <td><p contentEditable="true"></p></td>
-                    <td><p contentEditable="true"></p></td>
+                    <td><CustomTimePicker
+                        setTime={value => setValue(`proposals.${proposId}.hourMonday`,value)}
+                        value={formData.proposals?.[proposId]?.hourMonday}
+                    /></td>
+                    <td><CustomTimePicker
+                        setTime={value => setValue(`proposals.${proposId}.hourTuesday`,value)}
+                        value={formData.proposals?.[proposId]?.hourTuesday}
+                    /></td>
+                    <td><CustomTimePicker
+                        setTime={value => setValue(`proposals.${proposId}.hourWednesday`,value)}
+                        value={formData.proposals?.[proposId]?.hourWednesday}
+                    /></td>
+                    <td><CustomTimePicker
+                        setTime={value => setValue(`proposals.${proposId}.hourThursday`,value)}
+                        value={formData.proposals?.[proposId]?.hourThursday}
+                    /></td>
+                    <td><CustomTimePicker
+                        setTime={value => setValue(`proposals.${proposId}.hourFriday`,value)}
+                        value={formData.proposals?.[proposId]?.hourFriday}
+                    /></td>
                 </tr>
-            </tbody>
-            
+            </tbody>    
         </table>
-        <div className='TrainingProposal-chooseDate-container'>
-            <div className='TrainingProposal-chooseDate'>Date de début<CustomDatePicker className="TrainingProposal-date" placeholder="Date de début"/></div>
-            <div className='TrainingProposal-chooseDate'>Date de fin<CustomDatePicker className="TrainingProposal-date" /></div>
+        <div className='tp-dates-container'>
+            <div>
+                <p>Date de début {proposId}</p>
+                <CustomDatePicker
+                    className={errors.proposals?.[proposId]?.dateStart ? "input-error" : ""}
+                    disabledSlots={[]}
+                    // disableAfter={formData.proposals?.[key] ? formData.proposals?.[key]?.dateEnd : null}
+                    setDate={(value) => setValue(`proposals.${proposId}.dateStart`,value, errors.proposals?.[proposId]?.dateStart ? { shouldValproposIdate: true } : { shouldValproposIdate: false })}
+                    value={formData?.proposals?.[proposId]?.dateStart}
+                    {...register(`proposals.${proposId}.dateStart`, { required: "La date de début est obligatoire." })}
+                />
+            </div>
+            <div>
+                <p>Date de fin</p>
+                <CustomDatePicker
+                    className={errors.proposals?.[proposId]?.dateEnd ? "input-error" : ""}
+                    disabledSlots={[]}
+                    // disableBefore={formData.proposals?.[key] ? formData.proposals?.[key]?.dateStart : null}
+                    setDate={(value) => setValue(`proposals.${proposId}.dateEnd`,value, errors.proposals?.[proposId]?.dateEnd ? { shouldValproposIdate: true } : { shouldValproposIdate: false })}
+                    value={formData?.proposals?.[proposId]?.dateEnd}
+                    {...register(`proposals.${proposId}.dateEnd`, { required: "La date de fin est obligatoire." })}
+                />
+            </div>
         </div>
     </div>
   )
